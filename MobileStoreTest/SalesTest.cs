18399589daf76a3751cffe7e0e8e41b41687025d
rites <rites@DESKTOP-C203G3S>
2021-07-12 00:30:52 +0530

    ref #Commit to Github

---
+++ MobileStoreTest/SalesTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileStoreTest
+{
+    public class SalesTest
+    {
+        DateTime fromDate = Convert.ToDateTime("01/01/2021 00:00:00");
+        DateTime toDate = Convert.ToDateTime("07/01/2021 00:00:00");
+
+
+    }
+}
